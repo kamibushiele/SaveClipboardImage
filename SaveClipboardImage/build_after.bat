22191b1f39c18a22bf00ac5854507b873646ce24
kamibushiele <84666539+kamibushiele@users.noreply.github.com>
2022-10-29 22:08:58 +0900

    ビルド後コマンド外部化

--- SaveClipboardImage/build_after.bat
+++ SaveClipboardImage/build_after.bat
@@ -1,20 +1,20 @@
 @REM readme, license等コピー
-md $(TargetDir)Licenses
-xcopy /y $(SolutionDir)\Licenses\* $(TargetDir)Licenses
-xcopy /y $(SolutionDir)\License.txt $(TargetDir)
-xcopy /y $(SolutionDir)\Readme.md $(TargetDir)
-xcopy /y $(ProjectDir)\bat\* $(TargetDir)
+md %TargetDir%Licenses
+xcopy /y %SolutionDir%\Licenses\* %TargetDir%Licenses
+xcopy /y %SolutionDir%\License.txt %TargetDir%
+xcopy /y %SolutionDir%\Readme.md %TargetDir%
+xcopy /y $(ProjectDir)\bat\* %TargetDir%
 @REM 不要ファイル削除
-@REM del $(TargetDir)*.xml
-del $(TargetDir)*.pdb
+@REM del %TargetDir%*.xml
+del %TargetDir%*.pdb
 
 @REM 実行ファイル配布用zip化。
 @REM zipコマンドが使える場合は 以下gotoをコメントアウト
 @REM goto END
-cd $(TargetDir)\..\
-md $(ProjectName)
-echo aaa $(targetDir)
-xcopy $(targetDir) /e /y $(ProjectName)\
-zip -r $(ProjectName).zip $(ProjectName)\
-rd /s /q $(ProjectName)\
+cd %TargetDir%\..\
+md %ProjectName%
+echo aaa %TargetDir%
+xcopy %TargetDir% /e /y %ProjectName%\
+zip -r %ProjectName%.zip %ProjectName%\
+rd /s /q %ProjectName%\
 :END
